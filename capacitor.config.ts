fd469868268bb52a778315cc20f02f32e03ddaf3
Lucas Pasqualoni <Lucaspasq@Gmail.com>
2025-10-05 12:15:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'petrichor-app',
+  webDir: 'www'
+};
+
+export default config;
